1d44fcb693412342d31e79176be1391eee0d6343
Muchai248 <muchaimark6@gmail.com>
2023-09-20 23:50:32 +0300

    new commit

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -3,11 +3,13 @@ import json
 
 class GetRequester:
 
-    def __init__(self, url):
-        self.url = url
+    def __init__(self, URL):
+        URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+        self.url = URL
 
-    def get_response_body(self):
-        pass
+    def get_response_body(self,URL):
+     response = requests.get(URL)
+     return response.content
 
     def load_json(self):
         pass
